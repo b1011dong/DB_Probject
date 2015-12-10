f76a5e9e9d7b7a23873048641ff64f905a0e6916
ParkDongwon <b1011dong@gmail.com>
2015-12-11 06:07:58 +0900

    암걸리겠다 으으

--- src/gui/main/AssignmentListPanel.java
+++ src/gui/main/AssignmentListPanel.java
@@ -1,13 +1,23 @@
 package gui.main;
 
+import javax.swing.JLabel;
+
 import gui.simple.SimpleJPanel;
 
 public class AssignmentListPanel extends SimpleJPanel{
 
 	private static final long serialVersionUID = 2382632880666551084L;
-
+	
+	private JLabel subjectName;
+	private JLabel professorName;
+	
 	public AssignmentListPanel() {
 		
+		addComponents();
 	}
 
+	public void addComponents() {
+		
+	}
+	
 }
